8298e5b301f924f2a890fdf47ddcd2ab17fbfb69
Ines P. Chavarri <inespchavarri@gmail.com>
2019-09-22 22:26:39 +0200

    relacion de funciones empleadas con near

---
+++ near_funciones.py
@@ -0,0 +1,51 @@
+def GetCompaniesNear(geopoint, max_meters=1000):
+
+    # Función para obtener el número de empresas situadas a un km a la redonda del punto seleccionado
+
+    return len(list(db.clean_companies.find({
+        "geo": {
+            "$near": {
+                "$geometry": geopoint,
+                "$maxDistance": max_meters
+            }
+        }
+    })))
+
+
+def getEmployees(geopoint, max_meters=1000):
+
+    # Función para obtener el número de empleados que trabajan en las diferentes empresas situadas a un km a la redonda
+
+    info = list(db.clean_companies.find({
+        "geo": {
+            "$near": {
+                "$geometry": geopoint,
+                "$maxDistance": max_meters
+            }
+        }
+    }))
+
+    total = 0
+    for e in info:
+        total += e["number_of_employees"]
+    return total
+
+
+def getMoney(geopoint, max_meters=1000):
+
+    # Función para calcular el dinero total de las empresas situadas a un km a la redonda
+
+    info = list(db.clean_companies.find({
+        "geo": {
+            "$near": {
+                "$geometry": geopoint,
+                "$maxDistance": max_meters
+            }
+        }
+    }))
+
+    total = 0
+    for e in info:
+        total += e["total_amount_raised"]
+    
+    return total
